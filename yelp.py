d2a25b31d8f8fdc070318c067d5543eab662415a
AbduGadir <AbduGadir@users.noreply.github.com>
2018-04-18 02:10:33 -0400

    Update yelp.py

--- yelp.py
+++ yelp.py
@@ -1,3 +1,10 @@
+'''
+不用修改源文件
+直接调用
+python yelp.py #number
+
+'''
+
 from yelpapi import YelpAPI
 import json
 import sys
@@ -6,21 +13,18 @@ import time
 key = "api-key"
 yelp_api = YelpAPI(key)
 
-result = {}
+result = []
 
 temp = open("business"+sys.argv[1]+".json","wt")
 
-with open(sys.argv[1],'r') as infile:
-	phones = infile.read().split(',')
+data = json.load(open(sys.argv[1]))
     
-for obj in phones:
-	phone = "+1" + json.loads(obj)["PHONE"]
+for line in phones:
+	phone = "+1" + line["PHONE"]
 	response = yelp_api.phone_search_query(phone = phone)
-	result.update(response)
-	json.dump(response,temp)
-	time.sleep(1)
-	
+	result.append(response)
+	time.sleep(1)	
 
-with open(sys.argv[1]+'business.json', 'w') as outfile:
+with open("business_" + sys.argv[1] + ".json", 'w') as outfile:
     json.dump(result, outfile)
 
