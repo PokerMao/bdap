6ad71b16c48a2e0b4ac5c5acbfa74e5edca758cb
AbduGadir <AbduGadir@users.noreply.github.com>
2018-05-03 18:09:51 -0400

    Update settings.py

--- Website2/restaurant-information/website/settings.py
+++ Website2/restaurant-information/website/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')pqo6)f8glk38i(-87%z_v(76yd3&_cg^__$rq=#59^j=^)z+r'
+SECRET_KEY = 'secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
